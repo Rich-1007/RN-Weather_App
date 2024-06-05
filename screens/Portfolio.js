f4215cd75d1855ba3a8e6ebd8bb43378f515713c
Rich-1007 <ghricheak@gmail.com>
2024-06-05 23:25:30 +0530

    Portfolio Updates.

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -15,3 +15,49 @@ const githubImg = require("../assets/github-icons-1.png")
 
 
 
+
+
+export default function Portfolio() {
+  return (
+    <View style={styles.container} className="bg-[#1C1A1A]  ">
+      {/* <StatusBar style="dark" /> */}
+      <SafeAreaView >
+        <ScrollView >
+          <View className="flex-1 h-50 w-full ">
+            <View className="pt-20">
+              <Text className="text-3xl text-[#C05621] font-semibold pb-10  ">Hi There,</Text>
+              <Text className="text-white text-lg">
+                I am
+                <Text className="font-bold text-xl"> Hricheak Ghosh </Text>
+              </Text>
+              <Text className="text-white font-bold text-xl">
+                Frontend Developer
+              </Text>
+              <Text className="text-white font-bold text-xl">
+                based in Hyderabad, Telangana..
+              </Text>
+              <View className="flex flex-row gap-4 py-5">
+                <Image source={linkedinImg} className="h-7 w-7 rounded-full" />
+                <Image source={githubImg} className="h-7 w-7 rounded-full" />
+
+              </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
