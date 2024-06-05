71faab39d00b932e1d32467c2685d66e2e7b0120
Rich-1007 <ghricheak@gmail.com>
2024-06-05 23:23:40 +0530

    Portfolio UI.

---
+++ screens/Portfolio.js
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+const logoImg = require("./adaptive-icon.png")
+const profileImg = require("../assets/Profile-Photo (2).png")
+const linkedinImg = require("../assets/Linkedin-icon-1.png")
+const githubImg = require("../assets/github-icons-1.png")
+
+
+
