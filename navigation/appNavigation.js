ca863faed7f673fbfd91c35f94228440ed335e9a
Rich-1007 <ghricheak@gmail.com>
2024-06-06 00:13:52 +0530

    Created a Drawer Navigator

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -2,16 +2,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import HomeScreen from "../screens/HomeScreen";
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Portfolio from "../screens/Portfolio";
 
-const Stack = createStackNavigator();
+
+// const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        {/* <Drawer.Screen name="Portfolio" component={Portfolio}/> */}
-      </Stack.Navigator>
+
+      <Drawer.Navigator>
+
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} /> */}
+        <Drawer.Screen name="Portfolio" component={Portfolio} />
+        <Drawer.Screen name="Weather App" component={HomeScreen} 
+        // options={{ headerShown: false }}
+         />
+      </Drawer.Navigator>
+
     </NavigationContainer>
   );
 };
--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -8,7 +8,7 @@ import {
   Text,
   View,
 } from "react-native";
-const logoImg = require("./adaptive-icon.png")
+// const logoImg = require("./adaptive-icon.png")
 const profileImg = require("../assets/Profile-Photo (2).png")
 const linkedinImg = require("../assets/Linkedin-icon-1.png")
 const githubImg = require("../assets/github-icons-1.png")
