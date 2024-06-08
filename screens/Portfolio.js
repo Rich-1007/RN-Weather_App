1db1d3281e9d7d5f0d11bbd7576818447369f61b
Rich-1007 <ghricheak@gmail.com>
2024-06-09 00:25:21 +0530

    Ui updates

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -16,12 +16,12 @@ const githubImg = require("../assets/github-icons-1.png")
 const icon1 = require("../assets/Tech-Stack/html-5--v1.png")
 const icon2 = require("../assets/Tech-Stack/css3.png")
 const icon3 = require("../assets/Tech-Stack/bootstrap.png")
-const icon4 = require("../assets/Tech-Stack/Tailwind_CSS_Logo.svg.png")
+const icon4 = require("../assets/Tech-Stack/Tailwind CSS.png")
 const icon5 = require("../assets/Tech-Stack/javascript--v1.png")
 
 
 const icon6 = require("../assets/Tech-Stack/mysql-logo.png")
-const icon7 = require("../assets/Tech-Stack/react.png")
+const icon7 = require("../assets/Tech-Stack/React.png")
 const icon8 = require("../assets/Tech-Stack/nodejs.png")
 const icon9 = require("../assets/Tech-Stack/ios.png")
 const icon10 = require("../assets/Tech-Stack/android.png")
@@ -35,9 +35,9 @@ export default function Portfolio() {
       {/* <StatusBar style="dark" /> */}
       <SafeAreaView >
         <ScrollView >
-          <View className="flex-1 h-50 w-full ">
+          <View className="flex-1 h-50 w-full px-3">
             <View className="pt-20">
-              <Text className="text-3xl text-[#C05621] font-semibold pb-10  ">Hi There,</Text>
+              <Text className="text-3xl text-[#C05621] font-semibold pb-10">Hi There,</Text>
               <Text className="text-white text-lg">
                 I am
                 <Text className="font-bold text-xl"> Hricheak Ghosh </Text>
@@ -48,47 +48,47 @@ export default function Portfolio() {
               <Text className="text-white font-bold text-xl">
                 based in Hyderabad, Telangana..
               </Text>
-              <View className="flex flex-row gap-4 py-5">
-                <Image source={linkedinImg} className="h-7 w-7 rounded-full" />
-                <Image source={githubImg} className="h-7 w-7 rounded-full" />
+              <View className="flex flex-row gap-4 py-3">
+                <Image source={linkedinImg} className="h-12 w-12 rounded-full" />
+                <Image source={githubImg} className="h-12 w-12 rounded-full" />
 
               </View>
 
-              <View className="flex flex-row py-6  items-center justify-around ">
+              <View className="flex flex-row h-24 w-1/2 items-center justify-between">
                 <Button title="Hire Me" color="#C05621" />
                 <Button title="Download CV" color="#353434" />
               </View>
 
-              <View className="flex flex-row items-center  ">
+              <View className="flex flex-row items-center flex-wrap gap-2 py-8">
                 <Text className="text-white font-semibold py-6 text-lg">Tech Stack </Text>
-                <View className="h-28 border-green-400 border-2 bg-green-400"></View>
-                <View className="flex flex-row  flex-wrap gap-1">
-                  <Image source={icon1} className="h-14 w-14 " />
-                  <Image source={icon2} className="h-14 w-14" />
-                  <Image source={icon3} className="h-14 w-14" />
-                  <Image source={icon4} className="h-14 w-14" />
-                  <Image source={icon5} className="h-14 w-14" />
+
+                <View className="h-16 border-2 rounded-3xl border-green-400"></View>
+
+                <Image source={icon1} className="h-14 w-14 " />
+                <Image source={icon2} className="h-14 w-14" />
+                <Image source={icon3} className="h-14 w-14" />
+                <Image source={icon4} className="h-14 w-14" />
+                <Image source={icon5} className="h-14 w-14" />
 
 
-                  <View className="flex flex-row  flex-wrap gap-1">
-
-                    <Image source={icon6} className="h-14 w-14" />
-                    <Image source={icon7} className="h-14 w-14" />
-                    <Image source={icon8} className="h-14 w-14" />
-                    <Image source={icon9} className="h-14 w-14" />
-                    <Image source={icon10} className="h-14 w-14" />
-                  </View>
-                </View>
-
+                <Image source={icon6} className="h-14 w-14" />
+                <Image source={icon7} className="h-14 w-14 " />
+                <Image source={icon8} className="h-14 w-14" />
+                <Image source={icon9} className="h-14 w-14" />
+                <Image source={icon10} className="h-14 w-14"/>
 
               </View>
             </View>
 
-            <View className="bg-[#C05621]  my-10 h-60 w-60 rounded-full overflow-hidden flex justify-center items-center">
-              <Image source={profileImg} className="h-60 w-60 " />
+            <View className="flex justify-center items-center">
 
-
+              <View className="bg-[#C05621]  my-10 h-60 w-60 rounded-full overflow-hidden flex justify-center items-center">
+                <Image source={profileImg} className="h-60 w-60 " />
+              </View>
             </View>
+
+
+
           </View>
         </ScrollView>
       </SafeAreaView>
